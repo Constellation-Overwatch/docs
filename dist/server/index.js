a88460ca43ce3f164dd7ef5455f8fd5114ea85ea
tobalo <tobalo@yeetum.com>
2025-12-29 08:22:30 -0600

    config: rm dist

--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-import { G, F } from "./assets/worker-entry-CaO_sJbc.js";
-import "node:events";
-import "node:stream";
-import "node:async_hooks";
-import "node:stream/web";
-export {
-  G as createServerEntry,
-  F as default
-};
